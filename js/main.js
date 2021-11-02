c30ad3f2a03413e6221e9a7ae184e7d6e4a1dcbe
Justin Chapman <justinandrewchapman@gmail.com>
2021-11-02 15:15:05 -0400

    Updating progress, adding image/flip/compare

--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
 // constants
-let img = document.getElementsByClassName("board")
-let images = [...img]
+let images = [];
+const img = document.querySelector(".set");
+img.addEventListener("click", imgFlip);
 // state
-let imgSelect = []
+let imgChoices = [];
+let matchCounter = 0;
 
 //functions
-function init(){
-    
+function init() {
+  shuffle();
 }
-function win()
-function imgFlip()
-
-function shuffle(img){
-    let idx = img.length, randomIdx;
-    while(idx != 0){
-        randomIdx = Math.floor(Math.random() * idx); idx--;
-        [img[idx], img[randomIdx]] = [img[randomIdx], img[idx]]
-    }
-    return img;
+function win() {
+  if (matchCounter === 8) {
+  }
 }
 
-function match()
+function imgFlip(i) {
+  if (i.target.parentNode.getAttribute("disabled")) return;
+  imgChoices.push(i.target.parentNode.getAttribute("data-name"));
+  i.target.parentNode.setAttribute("disabled", true);
+  match();
+}
+function shuffle() {
+  for (let i = img.length - 1; i > 0; i--) {
+    let idx = Math.floor(Math.random() * (i + 1));
+    let newImg = images[i];
+    images[i] = images[idx];
+    images[idx] = newImg;
+  }
+  return images;
+}
 
-
-//event listeners
-//click events for card selects
-//click events for buttons
-
-// event clicking the element, a For loop can iterate through the array
-
-
-
-//element selectors
+function match() {
+  if (imgChoices.length < 2) return;
+  if (imgChoices[0] === imgChoices[1]) {
+    matchCounter++;
+    imgChoices.splice(0, imgChoices.length);
+    win();
+  } else {
+    imgChoices.setAttribute("disabled", false);
+    imgChoices.splice(0, imgChoices.length);
+  }
+}
